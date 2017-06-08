28fb3bf72331cbcfb0a2bb87ca4f747492039623
smcgee <sean.mcgee@kannuu.com>
2017-06-08 11:48:26 -0500

    Updates to output

--- app_async.py
+++ app_async.py
@@ -3,6 +3,8 @@ from aiohttp import ClientSession
 import aiofiles
 import json
 import csv
+import pandas as pd
+import os
 
 headers = {
 	'Content-Type': 'application/json; charset=UTF-8',
@@ -16,18 +18,19 @@ headers = {
 def normalize_responses(result):
 	json_parsed = json.loads(result)
 
-	validated_data = {}
-	validated_data['address'] = json_parsed.get('d').get('sMatchAddr')
-	validated_data['state'] = json_parsed.get('d').get('sStateCode')
-	validated_data['country'] = json_parsed.get('d').get('sCountyCode')
-	validated_data['msa'] = json_parsed.get('d').get('sMSACode')
-	validated_data['tract'] = json_parsed.get('d').get('sTractCode')
+	validated_data = []
+	address = json_parsed.get('d').get('sMatchAddr')
+	state = json_parsed.get('d').get('sStateCode')
+	country = json_parsed.get('d').get('sCountyCode')
+	msa = json_parsed.get('d').get('sMSACode')
+	tract = json_parsed.get('d').get('sTractCode')
+	data_array = [address, state, country, msa, tract]
+	validated_data.append(data_array)
 
-	json_validated_data = json.dumps(validated_data)
-	return json_validated_data
+	return validated_data
 
 async def main():
-	with open('TestData100.csv') as csvfile:
+	with open('TestData1000.csv') as csvfile:
 		has_header = csv.Sniffer().has_header(csvfile.read(1024))
 		csvfile.seek(0)
 		incsv = csv.reader(csvfile, delimiter=',')
@@ -48,12 +51,17 @@ async def getData(address, session):
 	async with session.post(url, headers=headers, data=data) as response:
 		data = await response.text()
 		result = normalize_responses(data)
-	async with aiofiles.open('data.json', 'a') as file:
-		await file.write(result)
+		df =  pd.DataFrame(result)
+		output_size = os.path.getsize('data.csv')
+	async with aiofiles.open('data.csv', 'a', newline="") as file:
+		if output_size > 0:
+			await file.write(df.to_csv(index=False, header=False))
+		else:
+			df.columns = ["Matched Address", "State", "Country", "MSA", "Tract"]
+			await file.write(df.to_csv(index=False, header=True))
 
 # Program starts here...
 if __name__ == '__main__':
-
 	loop = asyncio.get_event_loop()
 	future = asyncio.ensure_future(main())
 	loop.run_until_complete(future)
